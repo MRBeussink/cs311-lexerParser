728f8a0fb9b664665884bcea3ed4f9f657708a98
MRBeussink <MRBeussink@gmail.com>
2016-10-02 00:48:07 -0500

    Working Parser
    Parsing like a BA
    
    - still need clean-up -

--- Main.java
+++ Main.java
@@ -1,14 +1,32 @@
 /**
  * Created by markbeussink on 9/30/16.
  */
+
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-       Lexer lexer = new Lexer("for (int i = 0; i < count; i++)");
+        //String forLoop = "for (int i = 0; (i < count; i++)";
+        String input;
+        char c;
+        Scanner scanner = new Scanner(System.in);
 
         do {
-            lexer.lex();
-        } while (!lexer.isAtEnd());
-        
+            input = "";
+            System.out.println("Enter a for loop header.");
+            System.out.println("i.e \"for (int i = 0; (i < count; i++)\"");
+            input = scanner.nextLine();
+
+            Parser prsr = new Parser(input);
+            try {
+                prsr.parse();
+            }
+            catch (Parser.ParseError e){
+                System.out.println();
+            }
+
+            System.out.println("Do you want to try another input? (y | n)");
+            c = scanner.nextLine().charAt(0);
+        } while (c != 'n');
     }
 }
--- Parser.java
+++ Parser.java
@@ -6,12 +6,18 @@ public class Parser {
     private Lexer.Token nextToken;  // holds the next token
     private Lexer lxr;        // holds the Lexer object
 
+    public class ParseError extends Exception{
+        public ParseError(String errorMessage){
+            super(errorMessage);
+        }
+    }
+
     public Parser(String input){
         lxr = new Lexer(input);
     }
 
     // Entry point for recusive parsing
-    public void parse(){
+    public void parse() throws ParseError{
         System.out.println("Beginning parse.");
 
         //for keyword
@@ -57,7 +63,7 @@ public class Parser {
         System.out.println("Finished parse.");
     }
 
-    public void varInit(){
+    public void varInit() throws ParseError{
         System.out.println("Enter <VarInit>");
 
         // [Type] id = value
@@ -110,44 +116,83 @@ public class Parser {
         System.out.println("Exit <VarInit>");
     }
 
-    public void condition(){
+    public void condition()  throws ParseError{
         System.out.println("Enter <Condition>");
+
+        // boolean literal
+        if (nextToken == Lexer.Token.BOOL_LIT){
+            nextToken = lxr.lex();
+        }
+        // boolean expression
+        else {
+            booleanExpression();
+        }
 
         System.out.println("Exit <Condition>");
     }
 
-    public void booleanLiteral(){
-        System.out.println("Enter <BooleanLiteral>");
-
-        System.out.println("Exit <BooleanLiteral>");
-    }
-
-    public void booleanExpression(){
+    public void booleanExpression() throws ParseError{
         System.out.println("Enter <BooleanExpression>");
+
+        // expr
+        expr();
+        // [<BooleanOp> <Expr>]
+        if (nextToken == Lexer.Token.LOGIC_OP || nextToken == Lexer.Token.RELATIONAL_OP){
+            nextToken = lxr.lex();
+            // <Expr
+            expr();
+        } else {
+            error();
+        }
 
         System.out.println("Exit <BooleanExpression>");
     }
 
-    public void expr(){
+    public void expr() throws ParseError{
         System.out.println("Enter <Expr>");
 
+        // [<PrePostOp>] <exp> [(<Infix_Op> <exp> | <PrePostOp>)]
+        if (nextToken == Lexer.Token.PREPOST_OP){
+            nextToken = lxr.lex();
+        }
+
+        exp();
+
+        if (nextToken == Lexer.Token.INFIX_OP){
+            nextToken = lxr.lex();
+            exp();
+        }
+        else if (nextToken == Lexer.Token.PREPOST_OP){
+            nextToken = lxr.lex();
+        }
         System.out.println("Exit <Expr>");
     }
 
-    public void exp(){
+    public void exp() throws ParseError{
         System.out.println("Enter <Exp>");
 
+        // <Term> | (<Expr>)
+        if (nextToken == Lexer.Token.ID || nextToken == Lexer.Token.VALUE){
+            nextToken = lxr.lex();
+        }
+        else if (nextToken == Lexer.Token.LEFT_PAREN){
+            nextToken = lxr.lex();
+            expr();
+            if (nextToken == Lexer.Token.RIGHT_PAREN){
+                nextToken = lxr.lex();
+            } else {
+                error();
+            }
+        }
+        else {
+            error();
+        }
         System.out.println("Exit <Exp>");
-
     }
 
-    public void term(){
-        System.out.println("Enter <Term>");
 
-        System.out.println("Exit <Term>");
-    }
-
-    public void error(){
+    public void error() throws ParseError{
         System.out.printf("ERROR: Invalid token: %s", nextToken.name());
+        throw new ParseError("Invalid token " + nextToken.name() + "\n");
     }
 }
